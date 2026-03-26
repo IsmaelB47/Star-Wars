0e42fa0df9aa2c090cba6038778e7ee10ef6597e
IsmaelB47 <ibabani@ivytech.edu>
2026-03-26 16:46:44 -0400

    Star Wars app with tabs and drawer navigation

---
+++ app/_layout.tsx
@@ -0,0 +1,32 @@
+import { Platform } from 'react-native';
+import { Tabs } from 'expo-router';
+import { Drawer } from 'expo-router/drawer';
+
+export default function RootLayout() {
+  if (Platform.OS === 'android') {
+    return (
+      <Drawer>
+        <Drawer.Screen
+          name="planets"
+          options={{ drawerLabel: 'Planets', title: 'Planets' }}
+        />
+        <Drawer.Screen
+          name="films"
+          options={{ drawerLabel: 'Films', title: 'Films' }}
+        />
+        <Drawer.Screen
+          name="spaceships"
+          options={{ drawerLabel: 'Spaceships', title: 'Spaceships' }}
+        />
+      </Drawer>
+    );
+  }
+
+  return (
+    <Tabs>
+      <Tabs.Screen name="planets" options={{ title: 'Planets' }} />
+      <Tabs.Screen name="films" options={{ title: 'Films' }} />
+      <Tabs.Screen name="spaceships" options={{ title: 'Spaceships' }} />
+    </Tabs>
+  );
+}
