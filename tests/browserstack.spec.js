c9db63fe6e45bfdd2d54af824ce30f0f7fb28de1
IsmaelB47 <ibabani@ivytech.edu>
2026-04-03 18:13:37 -0400

    Update BrowserStack workflow

--- tests/browserstack.spec.js
+++ tests/browserstack.spec.js
@@ -1,30 +1,30 @@
 const { test, expect, chromium } = require("@playwright/test");
 
-test("Star Wars app loads on BrowserStack", async () => {
+test("Star Wars app opens on BrowserStack", async () => {
   const caps = {
     browser: "chrome",
     browser_version: "latest",
     os: "os x",
     os_version: "ventura",
-    name: "Star Wars app test",
+    name: "Star Wars BrowserStack Test",
     build: "GitHub Actions Build",
     "browserstack.username": process.env.BROWSERSTACK_USERNAME,
     "browserstack.accessKey": process.env.BROWSERSTACK_ACCESS_KEY,
-    "browserstack.local": "true"
+    "browserstack.local": "true",
   };
 
   const wsEndpoint = `wss://cdp.browserstack.com/playwright?caps=${encodeURIComponent(
-    JSON.stringify(caps)
+    JSON.stringify(caps),
   )}`;
 
   const browser = await chromium.connect(wsEndpoint);
   const page = await browser.newPage();
 
-  await page.goto("http://bs-local.com:3000", {
-    waitUntil: "domcontentloaded"
+  await page.goto("http://bs-local.com:8081", {
+    waitUntil: "domcontentloaded",
   });
 
-  await expect(page).toHaveURL(/bs-local\.com:3000/);
+  await expect(page).toHaveURL(/bs-local\.com:8081/);
 
   await page.screenshot({ path: "browserstack-home.png", fullPage: true });
 
